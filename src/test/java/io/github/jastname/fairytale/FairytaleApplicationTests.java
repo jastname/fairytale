43820dd73950bde9abc47fb822c882af2c8ea485
jastname <si283172@gmail.com>
2026-04-04 11:10:51 +0900

    initial commit

---
+++ src/test/java/io/github/jastname/fairytale/FairytaleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jastname.fairytale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairytaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
